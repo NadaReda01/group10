3ecd4014cec9978b2e2ea604c2eb5fb3908dcd7b
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2022-01-02 22:09:25 +0200

    v1.2.1

---
+++ index.php
@@ -0,0 +1,129 @@
+<?php 
+
+  // echo "test message","test 2 ","test3";
+  # echo "test message","test 2 ","test3";
+
+
+  /*
+  used to print message 
+  to All Users 
+  */
+
+  // print ("test message using print method");
+
+
+//    $age = 20;
+//    $name = "root";
+   
+    //    $age = 30;
+
+
+    //    echo $age;     
+
+    //   ECHO $age;
+
+ 
+//    define('x',3.4,true);  // case - insensitive (flase by default)
+//     echo x;
+ //   echo constant('x');
+
+
+    $num1 =  2;    // int
+    $num2 = "2";   // str 
+
+    // /*
+    //  + 
+    //  - 
+    //  / 
+    //  * 
+    //  % 
+    //  **  
+    // */
+
+    // echo ($num1 - $num2);
+    // $age = 20; 
+
+
+    // echo ($num1 + $num2);
+
+    // echo   $num1 += $num2;         // $num1 =$num1 + $num2; 
+
+
+ 
+    //    var_dump($num1 > $num2);
+    //    var_dump($num1 >= $num2);
+      //  var_dump($num1 == $num2);      // true     (value)
+    //    var_dump($num1 === $num2);    // false      (value && datatype )
+
+    //  echo   $num1 + 1;
+
+    //    echo   $num1++;  // 2   //   $num1+1;    
+    //    echo $num1;   // 3 
+
+        //   echo ++$num1;
+
+        // echo $num1--;
+        // echo --$num1;
+
+
+
+      #AND
+      /*
+        cond1        op        con2     Result 
+         true        And        true    true 
+         true        And        false   false 
+         false       and        true    false 
+         false       and        false   false 
+     
+    */
+      
+      #OR  
+
+      /*
+        cond1        op        con2     Result 
+         true        or        true      true 
+         true        or        false     true 
+         false       or        true      true
+         false       or        false     false  
+     
+    */
+
+      #XOR 
+       /*
+        cond1        op        con2     Result 
+         true        xor        true      false 
+         true        xor        false     true
+         false       xor        true      true 
+         false       xor        false     false 
+     
+    */
+
+     //    !true = false  
+
+
+    //    $name = "root";
+    //    $message = "welcome , ";
+
+    //    echo    $message.$name;
+ 
+    // $status = 5;
+
+    // if($status == 1){
+
+    //  echo "test Message 1";
+    
+    //  }elseif($status == 2){
+    //     echo "test Message 2";
+    //  }
+    //  elseif($status == 3){
+    //     echo "test Message 3";
+    //  }
+    //  else{
+    //      echo 'status > 3';
+    //  }
+
+
+
+
+
+?>
