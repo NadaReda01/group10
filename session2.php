04f3bbce046c2a490b8dc69db1f299ff00d19b14
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2022-01-03 15:28:38 +0200

    v1.3.2

---
+++ session2.php
@@ -0,0 +1,341 @@
+<?php 
+
+ # Task ... 
+// Write a PHP program to calculate electricity bill .
+// Conditions:
+// For first 50 units – 3.50/unit
+// For next 100 units – 4.00/unit
+// For units above 150 – 6.50/unit
+// You can use conditional statements.
+
+
+
+// 170   
+
+// 50 * 3.50 
+// 100*4 
+// 20*6.50 
+
+
+// $variable = 3;
+//    switch ($variable) {
+//        case 1:
+//            # code...
+
+//            echo "test Message 1";
+//            break;
+       
+//            case ($variable >= 2 && $variable <= 5):
+//               // code .... 
+//               echo "2 to 5 ";
+//               break;
+   
+       
+//        default:
+//            # code...
+//            echo "test Message > 5 ";
+//            break;
+//    }
+
+
+
+
+// echo "test php message ";
+// echo "test php message ";
+// echo "test php message ";
+// echo "test php message ";
+// echo "test php message ";
+// echo "test php message ";
+// echo "test php message ";
+// echo "test php message ";
+
+
+//   for ($i=0; $i < 5 ; $i++) { 
+//       # code...
+//       echo "test php message <br>";
+//   }
+
+// $a = 0;
+// while ($a < 5) {
+//     # code...
+//     echo "test php message <br>";
+//     $a++;
+// }
+
+// $a = 11;
+// do {
+//     # code...
+//     echo 'test message';
+// } while ($a <= 10);
+
+
+
+
+
+//   for ($i=0; $i < 5 ; $i++) { 
+//       # code...
+//       if($i == 3 ){
+//           break;
+//       }
+//       echo "test php message <br>";
+//   }
+
+
+
+
+//   for ($i=0; $i < 5 ; $i++) { 
+//       # code...
+//       if($i == 3 ){
+//           continue;
+//       }
+//       echo $i."<br>";
+//   }
+
+
+// echo '
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Document</title>
+// </head>
+// <body>
+
+//     <h1>Welcome , Root Account</h1>
+    
+// </body>
+// </html>
+// ';
+
+
+
+// $num1 = 1;
+// $num2 = 2; 
+
+// echo ($num1+$num2);
+
+
+
+//    function addNumbers(){
+
+//    }
+
+
+
+// function message($name,$id = null)
+// {
+//     echo 'welcome ,'.$name.' & id = '.$id.'<br>';
+ 
+// }
+
+
+// // message("Root",2013);
+
+// // code 
+
+// message('X');
+
+
+
+
+//   function notify($name = null ){
+//       echo 'Send Mail to '.$name;
+//   }
+
+//   notify('ahmed');
+
+
+// declare(strict_types = 1);
+
+// function addNumbers(int $num1, int $num2):int{
+
+//     return  $num1+$num2+0.2;
+// }
+
+// echo  addNumbers(3,2);
+
+
+//    $age = 20; // global scope .... 
+
+
+//    // code 
+
+
+
+//    echo $age ;    // 20 
+
+
+
+
+// function test(){
+//     $age = 20; // local scope 
+// }
+
+
+
+// $age = 20;
+
+// function getAge(){
+//     global $age ; 
+//     $age *= 2;
+//     echo $age;
+// }
+
+// getAge();    // 20
+
+
+
+
+    //  function getNumber(){
+    //    static  $x = 0;
+    //      echo $x.'<br>';
+    //      $x++;
+    //  }
+
+    //  getNumber();  // 0  0   
+    //  getNumber();  // 0  1 
+    //  getNumber();  // 0  2   
+    //  getNumber();
+    //  getNumber();
+    //  getNumber(); 
+    //  getNumber();
+
+
+
+
+  //  $str = "   php   ";
+
+    // echo  strlen($str);
+     // echo  str_word_count($str);
+    //  echo      strrev($str);
+     // echo strpos($str,'course');
+    // echo str_replace('php','js',$str);
+
+
+    // $output =   explode(' ',$str);
+
+    // var_dump($output);
+
+
+    //  $student = ['x','y','z'];
+
+    // echo     implode(',',$student);
+
+
+    // echo strlen(rtrim($str));
+    // echo strlen(ltrim($str));
+    // echo strlen(trim($str));
+        
+    //    ltrim  
+    //    trim 
+
+
+    // $age = 20;
+
+   //  var_dump(is_int($age));
+
+   //    $gpa = 3.5;
+
+  //    var_dump(is_float($gpa));
+    
+    //   var_dump(is_numeric($age));
+
+
+    //  echo  pi();
+
+    //  2,5,0
+
+    //    echo min(2,0,7);
+        //   echo max(8,10,0);
+
+        // echo sqrt(64); 
+
+        // echo    round(0.7);
+
+
+    //    echo   rand(1,10);
+
+
+//     $student = ["Root",20,3.4];
+//     // $student = array("Root",20,3.4);
+
+//    //  print_r($student);
+
+// //    echo $student[1];
+
+//    //Array ( [0] => Root [1] => 20 [2] => 3.4 )
+
+
+//     foreach ($student as $k => $val) {
+//         # code...
+
+//         echo '* '.$k.' : '.$val.'<br>';
+ //   }
+
+
+// * 0 : Root
+// * 1 : 20
+// * 2 : 3.4
+
+
+
+    //   $student = ["name" => "Root","age" => 20,"gpa" => 3.4];
+    //   print_r($student);
+    //   Array ( [name] => Root [age] => 20 [gpa] => 3.4 )
+
+
+    //  echo $student['name'];
+
+       
+
+        //   foreach ($student as $k => $val) {
+        // # code...
+
+        // echo '* '.$k.' : '.$val.'<br>';
+        //  }
+
+// * name : Root
+// * age : 20
+// * gpa : 3.4
+
+//    echo  count($student);
+
+
+
+//      $students = [
+       
+//         "std1" => ["name" => "Root1","age" => 21,"gpa" => 3.4] , 
+//         "std2" => ["name" => "Root2","age" => 22,"gpa" => 3.5],
+//         "std3" => ["name" => "Root3","age" => 23,"gpa" => 3.6]
+              
+//      ];
+
+//   //  echo $students['std1']['name']; // root1;
+
+
+ 
+//     foreach ($students as $key => $value) {
+//         # code...
+//        foreach ($value as $subKey => $subArray) {
+//            # code...
+
+//            echo '* '.$subKey.' : '.$subArray.' |  ';
+
+//        }
+//        echo '<br>';
+//     }
+
+// * name : Root1 | * age : 21 | * gpa : 3.4 |
+// * name : Root2 | * age : 22 | * gpa : 3.5 |
+// * name : Root3 | * age : 23 | * gpa : 3.6 |
+
+
+?>
+
+
+
+
+
+
